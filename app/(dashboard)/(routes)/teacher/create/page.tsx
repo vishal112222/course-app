15cdfb54f19786d43e85ecf1ccfd2448b8b8f0c2
Akash kumar <vishumark45@gmail.com>
2024-12-23 23:30:55 +0530

    database setup and course page and error handling

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,13 +4,14 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import { Form, FormControl, FormDescription, FormField, FormLabel, FormMessage, FormItem
  } from "@/components/ui/form";
  import { Button } from "@/components/ui/button";
  import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
  const formSchema = z.object({
     title: z.string().min(1, { 
@@ -19,6 +20,7 @@ import Link from "next/link";
  })
 
 const CreatePage = () => {
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -28,8 +30,14 @@ const CreatePage = () => {
 
     const { isSubmitting, isValid } = form.formState;
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await axios.post("/api/courses", values);
+            router.push(`/teacher/courses/${response.data.id}`);
+            toast.success("Course created successfully");
+        } catch {
+            toast.error("Something went wrong! ");
+        }
     }
 
     return (
